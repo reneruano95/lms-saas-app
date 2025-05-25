cf6e11460f75eca33f116dd621929418f2d302d6
Rene Ruano <rene.ruano95@gmail.com>
2025-05-25 10:17:57 -0400

    feat: add CompanionCard and CompanionsList components; update main page layout with new sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,45 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
+import React from "react";
+import CompanionCard from "@/components/companion-card";
+import CompanionsList from "@/components/companions-list";
+import CallToAction from "@/components/call-to-action";
 
 const Page = () => {
-    return (
-        <div>
-            <h1 className="text-2xl underline">
-                Welcome to my Saas App
-            </h1>
-            <Button variant="default">
-                Let&apos;s get Started
-            </Button>
+  return (
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neral Network of the Brain"
+          subject="neuroscience"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Math Wizard"
+          topic="Derivates and Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
 
-        </div>
-    )
-}
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <CallToAction />
+      </section>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
